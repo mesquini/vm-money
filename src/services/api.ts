1013b9ac92705dccdef3cc5d13b784d485df08bc
Mesquini <mesquini@live.com>
2022-01-14 11:52:37 -0300

    fix env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.URL_SITE || 'http://localhost:3000'
+const baseURL = process.env.REACT_APP_URL_SITE || 'http://localhost:3000'
 
 export const api = axios.create({
   baseURL: `${baseURL}/api`
